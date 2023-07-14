37bd32297704f319b8e4b55d6b0a5ee097969069
Pranita <pfulsundar8@gmail.com>
2023-07-15 04:03:45 +0530

    feat: cabModal

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,10 @@
+import { CabDetailsModal } from "../../components";
 import styles from "./login.module.css";
 
 export const Login = () => {
   return (
     <div>
+      <CabDetailsModal />
       <h1 className={styles.header}>Login Page</h1>
     </div>
   );

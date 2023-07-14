a80ca8cc6b29a08277e6bc994ce77014c4e92cb5
Wadad Parker <wadadparker5@gmail.com>
2023-07-15 02:36:23 +0530

    feat: header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,11 @@
 import styles from "./home.module.css";
 
+import { Header } from "../../components/Header/Header";
+
 export const Home = () => {
   return (
     <div>
+      <Header />
       <h1>Home Page</h1>
     </div>
   );

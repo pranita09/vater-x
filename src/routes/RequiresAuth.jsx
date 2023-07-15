4a10a02b6727d653b4e47809e5880cb6055a0012
Wadad Parker <wadadparker5@gmail.com>
2023-07-15 16:13:50 +0530

    feat: private pages authorization

--- src/routes/RequiresAuth.jsx
+++ src/routes/RequiresAuth.jsx
@@ -1,1 +1,12 @@
-export const RequiresAuth = () => {};
+import { useData } from "../contexts/DataContext";
+import { Navigate } from "react-router-dom";
+
+export const RequiresAuth=({children})=>
+{
+    const {state:{isLoggedIn}}=useData();
+    return isLoggedIn ? (
+        children
+    ) : (
+        <Navigate to="/"/>
+    )
+}

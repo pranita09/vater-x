ce1797bdc88bd4b9ef87ba5aa8f169789485fde3
Wadad Parker <wadadparker5@gmail.com>
2023-07-15 07:43:14 +0530

    feat: cabs routing

--- src/pages/Cabs/Cabs.jsx
+++ src/pages/Cabs/Cabs.jsx
@@ -1,8 +1,31 @@
 import styles from "./cabs.module.css";
+import headerStyles from "../Drivers/drivers.module.css";
+import { useEffect } from "react";
+
+import {CiSearch} from "react-icons/ci"
+
+import { actionTypes } from "../../utils/constants";
+import {useData} from "../../contexts/DataContext";
+import { DriverCard } from "../../components/DriverCard/DriverCard";
+import {Header} from "../../components/Header/Header";
 
 export const Cabs = () => {
+  const {state,dispatch,getDrivers, getAllCabs,searchedCabs}=useData();
+  const {drivers, cabs, searchCabs}=state;
+
+  const { SEARCH_CABS } = actionTypes;
+
+  useEffect(()=>{getAllCabs()},[]);
   return (
-    <div>
+    <div className={headerStyles[`driver-container`]}>
+      <Header />
+      <header className={headerStyles.header}>
+        <span className={headerStyles.searchBar}>
+        <CiSearch className={headerStyles.icon}/>
+        <input value={searchCabs} className={headerStyles.input} type="search" placeholder="Search Cab" onChange={(e)=>dispatch({type:SEARCH_CABS,payload:e.target.value})}/>
+        </span>
+        <button className={headerStyles.button}>ADD CAB</button>
+      </header>
       <h1>Cabs Page</h1>
     </div>
   );

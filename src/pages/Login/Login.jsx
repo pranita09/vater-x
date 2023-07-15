0f68c416fcabccc9bfbac6a7bc38683fa7667427
Pranita <pfulsundar8@gmail.com>
2023-07-15 07:19:15 +0530

    feat: added toast message

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,10 @@
-import { DriverModal } from "../../components";
+import { Header } from "../../components";
 import styles from "./login.module.css";
 
 export const Login = () => {
   return (
     <div>
-      <DriverModal />
+      <Header />
       <h1 className={styles.header}>Login Page</h1>
     </div>
   );
--- src/utils/utilFunctions.js
+++ src/utils/utilFunctions.js
@@ -1,9 +1,11 @@
+import { toast } from "react-hot-toast";
+
 export const handleCopyToClipboard = (text) => {
-    const textField = document.createElement('textarea');
-    textField.innerText = text;
-    document.body.appendChild(textField);
-    textField.select();
-    document.execCommand('copy');
-    textField.remove();
-    alert('Copied to clipboard!');
-  };
+  const textField = document.createElement("textarea");
+  textField.innerText = text;
+  document.body.appendChild(textField);
+  textField.select();
+  document.execCommand("copy");
+  textField.remove();
+  toast.success("Copied to clipboard!");
+};

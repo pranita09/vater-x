a7d597048f525abefe9e9ee5afa926aa0fb5072c
Wadad Parker <wadadparker5@gmail.com>
2023-07-15 06:44:58 +0530

    style: search bar

--- src/pages/Drivers/Drivers.jsx
+++ src/pages/Drivers/Drivers.jsx
@@ -1,6 +1,8 @@
 import styles from "./drivers.module.css";
 import { useEffect } from "react";
 
+import {CiSearch} from "react-icons/ci"
+
 import {useData} from "../../contexts/DataContext";
 import { DriverCard } from "../../components/DriverCard/DriverCard";
 import {Header} from "../../components/Header/Header";
@@ -12,12 +14,18 @@ export const Drivers = () => {
   useEffect(()=>{getDrivers()},[]);
 
   return (
-    <div>
+    <div className={styles[`driver-container`]}>
       <Header />
-      <h1>Drivers Page</h1>
-      <ul>
+      <header className={styles.header}>
+        <span className={styles.searchBar}>
+        <CiSearch className={styles.icon}/>
+        <input className={styles.input} type="search" placeholder="Search Driver"/>
+        </span>
+        <button className={styles.button}>ADD DRIVER</button>
+      </header>
+      <ul className={styles[`list-container`]}>
       {drivers?.map(item=>(
-        <li key={item.id}>
+        <li key={item.id} className={styles[`list-item-container`]}>
           <DriverCard driver={item} />
         </li>
       ))}

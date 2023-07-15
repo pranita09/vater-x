69429e65d17442bd0e38b365b52eb71f4ea26005
Wadad Parker <wadadparker5@gmail.com>
2023-07-15 15:23:17 +0530

    style: login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,18 @@
-import { Header } from "../../components";
 import styles from "./login.module.css";
 
 export const Login = () => {
   return (
-    <div>
-      <Header />
-      <h1 className={styles.header}>Login Page</h1>
+    <div className={styles[`container`]}>
+      <h1 className={styles.header}>VATER-X</h1>
+      <div className={styles[`login-container`]}>
+        <main className={styles.main}>
+        <label className={styles.label}>Login</label>
+        <input className={styles.input} placeholder="Email"></input>
+        <input className={styles.input} type="password" placeholder="Password"></input>
+        <button className={styles.button}>LOGIN</button>
+        <button className={styles.button}>Login as Guest</button>
+        </main>
+      </div>
     </div>
   );
 };

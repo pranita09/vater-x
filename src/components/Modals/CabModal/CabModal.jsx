b595f00637e11d8214c7884b3fa8bd5da7195163
Pranita <pfulsundar8@gmail.com>
2023-07-15 06:15:22 +0530

    feat: add/edit cab modal

--- src/components/Modals/CabModal/CabModal.jsx
+++ src/components/Modals/CabModal/CabModal.jsx
@@ -1,9 +1,28 @@
 import styles from "./cabModal.module.css";
+import { BsCamera } from "react-icons/bs";
 
-export const CabModal = () => {
+export const CabModal = ({ isEdit }) => {
   return (
-    <div>
-      <h1>Cab Modal</h1>
+    <div className={styles.container}>
+      <p className={styles.header}>{isEdit ? "Edit" : "Add"} Cab Details</p>
+      <form className={styles.formInputs}>
+        <input type="text" placeholder="Enter name" />
+        <input type="text" placeholder="Registration Number" />
+        <input type="text" placeholder="Modal Number" />
+        <input type="text" placeholder="Color of the cab" />
+        <label>
+          <input type="file" accept="*/image" />
+          <span>
+            <BsCamera title="Add Cab Picture" />
+          </span>
+        </label>
+        <div className={styles.btns}>
+          <button type="submit" className={styles.submitBtn}>
+            {isEdit ? "Save" : "Add"}
+          </button>
+          <button className={styles.cancelBtn}>Cancel</button>
+        </div>
+      </form>
     </div>
   );
 };
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,10 @@
-import { DriverDetailsModal } from "../../components";
+import { CabModal } from "../../components";
 import styles from "./login.module.css";
 
 export const Login = () => {
   return (
     <div>
-      <DriverDetailsModal />
+      <CabModal />
       <h1 className={styles.header}>Login Page</h1>
     </div>
   );
